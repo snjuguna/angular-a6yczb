a509a7f49ca18ed7801e53bdd81b34b4a834ac1b
snjuguna <40172507+snjuguna@users.noreply.github.com>
2019-05-21 12:18:43 -0400

    Importing project from desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  preventDefault(){
+    event.preventDefault();
+  }
+  onClick(event:Event){
+    event.preventDefault();
+  }
 }
+
